ca1af70ea9bdaad7fb6fe8fcd3cc5dbc1d71839c
HeavensWisdom <153444308+HeavensWisdom@users.noreply.github.com>
2023-12-10 18:17:25 +0100

    Delete GPACalculator/com/lib/src directory

--- GPACalculator/com/lib/src/utils/GPAUtils.java
+++
@@ -1,8 +0,0 @@
-// GPAUtils.java
-package utils;
-
-public class GPAUtils {
-    public static double calculateGPA(double totalQualityPoint, int totalCourseUnit) {
-        return totalQualityPoint / totalCourseUnit;
-    }
-}
